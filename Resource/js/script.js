99ffdfbb3644b664b4747f4e5998a517dab7410f
Liamohth <45179229+Liamohth@users.noreply.github.com>
2020-07-19 19:25:02 -0700

    Add files via upload

---
+++ Resource/js/script.js
@@ -0,0 +1,12 @@
+
+document.querySelector("h5").addEventListener("click", function(){
+// var colorChange = document.querySelector("h5").style.color;
+// colorChange = "red";
+// document.body.style.backgroundColor = "red";
+document.querySelector("header").style.backgroundImage = "url('img/littleboy.jpg')";
+
+});
+
+// function changeColor() {
+//   body.textContent = "red";
+// }
